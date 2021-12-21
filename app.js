f4c2848afa778b3ee202bc871a7518cf082e7a18
Ray Messina <rmessina10@gmail.com>
2021-12-21 16:08:34 -0600

    objects

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var number1 = '5';
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+// const person: {
+//     name: string;
+//     age: number;
+// } =
+var person = {
+    name: 'Ray',
+    age: 50
+};
+console.log(person.name);
