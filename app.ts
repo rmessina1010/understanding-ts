3050b91a1eb77641a231a26c0b8a70c08cc82590
Ray Messina <rmessina10@gmail.com>
2021-12-21 16:31:29 -0600

    arrays

--- app.ts
+++ app.ts
@@ -2,9 +2,30 @@
 //     name: string;
 //     age: number;
 // } =
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role:[number, string]
+}={
     name: 'Ray',
-    age:50
+    age:50,
+    hobbies:['photography', 'music'],
+    role:[2,'author']
 };
 
+person.role.push('admin'); //exception
+// errors:
+// person.role= [7]
+// person.role= [7,'string','ppp']
+// person.role= []
+
+let favoriteActivities:string [];
+favoriteActivities=  ['art']
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    ///console.log(hobby.map());///!!!ERROR
+}
